cf3c0458913de8be129cf2561672a0c67fec3aa1
supercat95 <jibates@smu.edu>
2020-03-07 15:45:20 -0600

    Paw properly positioned and rotating
    Add paw pads?

--- paw.js
+++ paw.js
@@ -1,10 +1,9 @@
 class Paw {
     constructor() {
-        this.xPosLeft = 1080;
-        this.xPosRight = 1230;
-        this.xPosMid = (this.xPosLeft + this.xPosRight) / 2;
-        this.yPosBottom = window.innerHeight + 100;
-        this.yPosTop = 400;
+        this.xRad = 75;
+        this.xPosMid = 1155;
+        this.yPosBottom = 100;
+        this.yPosTop = -330;
         this.color = color(255, 179, 255);
         this.time = 0;
     }
@@ -13,18 +12,19 @@ class Paw {
         this.time += 1;
         fill(this.color);
         push();
-            // translate(this.xPosMid, this.window.innerHeight);
-            // rotate(sin(this.time * 0.1) * 1.5);
+            translate(this.xPosMid, window.innerHeight);=
+            rotate(sin(this.time * 0.2) * .2);
             beginShape();
-                vertex(this.xPosMid, this.yPosBottom);
-                vertex(this.xPosLeft, this.yPosBottom);
-                vertex(this.xPosLeft, 500);
-                curveVertex(this.xPosMid, this.yPosTop);
-                curveVertex(this.xPosRight, 500);
-                vertex(this.xPosRight, this.yPosBottom);
-                vertex(this.xPosMid, this.yPosBottom);
+                vertex(0, this.yPosBottom);
+                vertex(-1*this.xRad, this.yPosBottom);
+                vertex(-1*this.xRad, this.yPosTop + 100);
+                curveVertex(0, this.yPosTop);
+                curveVertex(this.xRad, this.yPosTop + 100);
+                vertex(this.xRad, this.yPosBottom);
+                vertex(0, this.yPosBottom);
             endShape();
         pop();
+        print(this.yPosTop, window.innerHeight);
     }
 
 }
